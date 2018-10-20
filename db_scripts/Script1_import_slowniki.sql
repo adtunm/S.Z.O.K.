94f13a02fdafa70f4dfc65517ea040fb9a21fd25
G. Nowak <g.nowak126@gmail.com>
2018-10-20 20:24:55 +0200

    Import słowniki

---
+++ db_scripts/Script1_import_slowniki.sql
@@ -0,0 +1,73 @@
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`KategorieWiekowe`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`KategorieWiekowe` (`id`, `nazwa`, `usunieto`) VALUES (1, 'N/A', NULL);
+INSERT INTO `SZOK`.`KategorieWiekowe` (`id`, `nazwa`, `usunieto`) VALUES (2, '7+', NULL);
+INSERT INTO `SZOK`.`KategorieWiekowe` (`id`, `nazwa`, `usunieto`) VALUES (3, '12+', NULL);
+INSERT INTO `SZOK`.`KategorieWiekowe` (`id`, `nazwa`, `usunieto`) VALUES (4, '15+', NULL);
+INSERT INTO `SZOK`.`KategorieWiekowe` (`id`, `nazwa`, `usunieto`) VALUES (5, '18+', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`RodzajeFilmow`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (1, 'Film akcji', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (2, 'Przygodowy', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (3, 'Sci-Fi', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (4, 'Fantasy', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (5, 'Komedia', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (6, 'Romans', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (7, 'Horror', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (8, 'Thriller', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (9, 'Dramat', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (10, 'Film animowany', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (11, 'Film biograficzny', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (12, 'Film historyczny', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (13, 'Western', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (14, 'Musical', NULL);
+INSERT INTO `SZOK`.`RodzajeFilmow` (`id`, `nazwa`, `usunieto`) VALUES (15, 'Film dokumentalny', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`WydarzeniaSpecjalne`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`WydarzeniaSpecjalne` (`id`, `nazwa`, `usunieto`) VALUES (1, 'Maraton', NULL);
+INSERT INTO `SZOK`.`WydarzeniaSpecjalne` (`id`, `nazwa`, `usunieto`) VALUES (2, 'Lejdis Night', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`TypySeansow`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`TypySeansow` (`id`, `nazwa`, `usunieto`) VALUES (1, '2D Napisy', NULL);
+INSERT INTO `SZOK`.`TypySeansow` (`id`, `nazwa`, `usunieto`) VALUES (2, '3D Napisy', NULL);
+INSERT INTO `SZOK`.`TypySeansow` (`id`, `nazwa`, `usunieto`) VALUES (3, '2D Dubbing', NULL);
+INSERT INTO `SZOK`.`TypySeansow` (`id`, `nazwa`, `usunieto`) VALUES (4, '3D Dubbing', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`RodzajeBiletow`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`RodzajeBiletow` (`id`, `nazwa`, `usunieto`) VALUES (1, 'Normalny', NULL);
+INSERT INTO `SZOK`.`RodzajeBiletow` (`id`, `nazwa`, `usunieto`) VALUES (2, 'Ulgowy', NULL);
+INSERT INTO `SZOK`.`RodzajeBiletow` (`id`, `nazwa`, `usunieto`) VALUES (3, 'Studencki', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`Role`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`Role` (`id`, `nazwa`, `usunieto`) VALUES (1, 'Administrator', NULL);
+INSERT INTO `SZOK`.`Role` (`id`, `nazwa`, `usunieto`) VALUES (2, 'Kierownik', NULL);
+INSERT INTO `SZOK`.`Role` (`id`, `nazwa`, `usunieto`) VALUES (3, 'Pracownik', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`TypyRzedow`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`TypyRzedow` (`id`, `nazwa`, `usunieto`) VALUES (1, 'zwykle', NULL);
+INSERT INTO `SZOK`.`TypyRzedow` (`id`, `nazwa`, `usunieto`) VALUES (2, 'tylko do kupna', NULL);
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`RodzajePlatnosci`
+-- -----------------------------------------------------
+INSERT INTO `SZOK`.`RodzajePlatnosci` (`id`, `nazwa`, `usunieto`) VALUES (1, 'Karta', NULL);
+INSERT INTO `SZOK`.`RodzajePlatnosci` (`id`, `nazwa`, `usunieto`) VALUES (2, 'Gotówka', NULL);
+INSERT INTO `SZOK`.`RodzajePlatnosci` (`id`, `nazwa`, `usunieto`) VALUES (3, 'Przelew', NULL);
+
+
+-- -----------------------------------------------------
+-- Data for table `SZOK`.`Uzytkownicy` '1305485a712608fdc4d2fd1780c72919f2f54cf288525814bff7120737f6ddad'
+-- -----------------------------------------------------
