5ea02b9ccb103e43539a257fed1562ca545ac0b3
G. Nowak <g.nowak126@gmail.com>
2018-10-23 19:14:21 +0200

    navigation bars for workersApp

---
+++ src/Constans/Role.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnowa
+ * Date: 23.10.2018
+ * Time: 09:34
+ */
+
+namespace App\Constans;
+
+class Role
+{
+    const ADMIN = 1;
+    const MANAGER = 2;
+    const WORKER = 3;
+}
